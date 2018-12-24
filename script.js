a5bf2af537a269a3ea2fb79ba37f3eb588da3c04
aminalakhsham <42694796+aminalakhsham@users.noreply.github.com>
2018-12-24 12:40:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// DON'T CHANGE ANYTHING BETWEEN HERE ---
+var time = 1000;
+
+var red = blinkLight(get('light-top'), 'red');
+var yellow = blinkLight(get('light-middle'), 'yellow');
+var green = blinkLight(get('light-bottom'), 'green');
+
+var red2 = blinkLight(get('light-top2'), 'red');
+var yellow2 = blinkLight(get('light-middle2'), 'yellow');
+var green2 = blinkLight(get('light-bottom2'), 'green');
+
+// blinkLight returns a function that switches a light on then waits a second
+// before switching it off again and calling its first argument as a 'callback'.
+// If no arguments are supplied the light just goes off.
+function blinkLight(element, colour) {
+  var blink = function(callback) {
+    element.classList.add(colour);
+    setTimeout(function() {
+      element.classList.remove(colour);
+      if (callback) {
+        callback();
+      }
+    }, time);
+  };
+
+  return blink;
+}
+
+function get(id) {
+  return document.getElementById(id);
+}
+
+// --- AND HERE.
+// Replace the example code below with your own solution:
+
+
+function light(call) {
+  // HINT: when red() finishes it calls the first argument as a function.
+  // So in this example the flow is red() -> wait a second -> green()
+  green(function() {
+    green(function(){
+      yellow(function(){
+        red(function(){
+          red(function(){
+            red(function(){
+              red(function(){
+               red();
+               yellow(function(){
+                call(call);
+               })
+              });
+            });
+          });
+        });
+      })
+    })
+  })
+}
+
+light(light);
+
+
+function light2(call) {
+  // Same as above except the flow is green2() -> wait a second -> red2()
+  red2(function() {
+    red2(function(){
+      red2(function(){
+        red2();
+        yellow2(function(){
+          green2(function(){
+            green2(function(){
+              yellow2(function(){
+                red2(function(){
+                  call(call);
+                })
+               })
+            });
+          });
+        })
+      })
+    })
+  })
+}
+light2(light2);
